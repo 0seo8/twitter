076e6ff23762d4db7b564cb01925b728dd181304
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-06 14:23:11 +0900

    settings firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,3 @@
-# FireBase
-
-- Firebase 와 AWS ampify 테스트 용으로 사용하는 것을 권장.
-
-> Firebase v9 & react-router-dom 업데이트 이슈 해결
-
-```sh
-$ npm i firebase@9.6.1
-$ npm i react-router-dom@5.3.0
-```
-
-## 1. 프로젝트 시작
-
-- 프로젝트 생성
-- github연동
-- firebase 프로젝트 시작
-
-### 1-1
-
-```shell
-$ yarn add firebase
-```
-
-`firebase.js`
-
-```js
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
@@ -38,4 +12,3 @@ const firebaseConfig = {
 }
 
 export default firebase.initializeApp(firebaseConfig)
-```
