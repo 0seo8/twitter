cc285e3e78c5196d2f8df985989b9774fd8e1523
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-19 15:07:38 +0900

    feat: realtime으로 데이터 가져오기

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,13 @@ import { authService } from 'fbase'
 function App() {
   const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userObj, setUserObj] = useState(null)
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true)
+        setUserObj(user)
       } else {
         setIsLoggedIn(false)
       }
@@ -22,7 +24,7 @@ function App() {
     <>
       <BrowserRouter>
         {init ? (
-          <AppRouter isLoggedIn={Boolean(isLoggedIn)} userObj={isLoggedIn} />
+          <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
         ) : (
           'Initializing...'
         )}
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,26 +2,33 @@ import { dbService } from 'fbase'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 
-function Home() {
+function Home({ userObj }) {
   const [nweet, setNweet] = useState('')
   const [nweets, setNweets] = useState([])
 
-  //db가져오기
-  const getNweets = async () => {
-    const dbNnweets = await dbService.collection('nweets').get()
-    //querysnapShot을 리턴하기 때문에 아래와 같이 처리를 해줘야합니다.
-    dbNnweets.forEach((document) => {
-      //객체로 만듦
-      const nweetObject = {
-        ...document.data(),
-        id: document.id,
-      }
-      setNweets((prev) => [nweetObject, ...prev])
-    })
-  }
+  //db가져오기 => 아래 주석은 오래된 내용
+  // const getNweets = async () => {
+  //   const dbNnweets = await dbService.collection('nweets').get()
+  //   //querysnapShot을 리턴하기 때문에 아래와 같이 처리를 해줘야합니다.
+  //   dbNnweets.forEach((document) => {
+  //     //객체로 만듦
+  //     const nweetObject = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     }
+  //     setNweets((prev) => [nweetObject, ...prev])
+  //   })
+  // }
 
   useEffect(() => {
-    getNweets()
+    //실시간으로 변화를 알려줌 => 리렌더링을 줄여줍니다.
+    dbService.collection('nweets').onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      setNweets(nweetArray)
+    })
   }, [])
 
   const onSubmit = async (e) => {
@@ -29,8 +36,9 @@ function Home() {
     //콜렉션에 data추가
     await dbService.collection('nweets').add({
       //collection의 key:value형태로
-      nweet,
+      text: nweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     })
     setNweet('')
   }
@@ -55,7 +63,7 @@ function Home() {
       <div>
         {nweets.map((nweet) => (
           <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
+            <h4>{nweet.text}</h4>
           </div>
         ))}
       </div>
